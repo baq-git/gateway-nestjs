6faf4bfc5da6f35f2fb7698af1f513ebdf926117
baq-git <hoquocbaocyber@gmail.com>
2026-01-29 11:25:34 +0700

    feat: change logic from idempotency service to idempotency interceptor

--- src/payment-receipt/payment-receipt.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PaymentReceiptPipe } from './payment-receipt.pipe';
-
-describe('PaymentReceiptPipe', () => {
-  it('should be defined', () => {
-    expect(new PaymentReceiptPipe()).toBeDefined();
-  });
-});
--- src/payment-receipt/payment-receipt.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-@Injectable()
-export class PaymentReceiptPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return value;
-  }
-}
